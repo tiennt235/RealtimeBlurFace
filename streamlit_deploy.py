33c30fd3c23f7cba90d07ff2636909cce92074d1
tiennt235 <tiennt235@gmail.com>
2022-06-23 09:59:25 +0700

    test

--- streamlit_deploy.py
+++ streamlit_deploy.py
@@ -4,10 +4,11 @@ from streamlit_webrtc import webrtc_streamer
 import av
 
 
-def video_frame_callback(frame):
-    img = blur_face(frame)
+class VideoProcessor:
+    def video_frame_callback(self, frame):
+        img = blur_face(frame)
 
-    return av.VideoFrame.from_ndarray(img, format="bgr24")
+        return av.VideoFrame.from_ndarray(img, format="bgr")
 
 
-webrtc_streamer(key="example")
+webrtc_streamer(key="example", video_processor_factory=VideoProcessor)
